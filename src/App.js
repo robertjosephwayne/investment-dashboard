b12c42a339c006ee4717ddf0a1412550ad8665f6
Robert <robertjwayne@gmail.com>
2021-01-24 04:13:15 -0800

    Add TopAppBar and StockTickerSearchContainer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import './App.css';
+import TopAppBar from './components/TopAppBar/TopAppBar';
+import StockTickerSearchContainer from './containers/StockTickerSearchContainer/StockTickerSearchContainer';
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <TopAppBar title="Investment Dashboard"></TopAppBar>
+      <div className="flex justify-center mt-6">
+        <StockTickerSearchContainer />
+      </div>
+    </div>
+  );
 }
 
 export default App;
