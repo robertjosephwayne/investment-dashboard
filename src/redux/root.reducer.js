1eced6afa84a1b6c99f9df788637750eb4bbe055
Robert <robertjwayne@gmail.com>
2021-01-25 03:29:28 -0800

    Create root reducer

--- src/redux/root.reducer.js
+++ src/redux/root.reducer.js
@@ -1,3 +1,19 @@
-import { combineReducers } from 'redux';
+import { combineReducers } from '@reduxjs/toolkit';
+import { combineEpics } from 'redux-observable';
+import { catchError } from 'rxjs/operators';
+import { fetchStockPricesEpic } from '../redux/StockTicker/StockTicker.epics';
+import stockTickerReducer from './StockTicker/StockTicker.reducer';
 
-export default combineReducers({});
+const epics = [fetchStockPricesEpic];
+
+export const rootEpic = (action$, store$, dependencies) =>
+  combineEpics(...epics)(action$, store$, dependencies).pipe(
+    catchError((error, source) => {
+      console.error(error);
+      return source;
+    })
+  );
+
+export const rootReducer = combineReducers({
+  stockTicker: stockTickerReducer,
+});
